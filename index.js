08e2f5c21bef20792a9ea59a955e21ecfbd10f60
RamMohan <ram222mohan@gmail.com>
2019-09-05 12:55:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const PubSub = {
+  events: {},
+  publish(event, data) {
+    if (!this.events[event]) return;
+    this.events[event].forEach(callback => callback(data));
+  },
+  subscribe(event, callback) {
+    if (!this.events[event]) this.events[event] = [];
+    this.events[event].push(callback);
+  },
+  unsubscribe(event) {
+      delete this.events[event]
+  }
+};
+export default PubSub;
